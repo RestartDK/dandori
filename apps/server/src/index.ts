134884d5916868c1a1f7fee348fe06448f607ace
Daniel Kumlin <danielkumlinwork@gmail.com>
2025-12-27 10:27:07 +0100

    fix: update server listen port to use environment variable or default to 3000

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -19,6 +19,6 @@ const app = new Elysia()
     return status(405);
   })
   .get("/", () => "OK")
-  .listen(3000, () => {
+  .listen(process.env.PORT || 3000, () => {
     console.log("Server is running on http://localhost:3000");
   });
