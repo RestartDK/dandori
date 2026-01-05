cdafb137c490f843d27543d98481750e6e73d8c5
Daniel Kumlin <danielkumlinwork@gmail.com>
2026-01-05 15:45:48 +0100

    fix: enhance API configuration to include auth creds

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,4 +1,8 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@server/index";
 
-export const api = treaty<App>(import.meta.env.VITE_SERVER_URL || "");
+export const api = treaty<App>(import.meta.env.VITE_SERVER_URL || "", {
+  fetch: {
+    credentials: "include",
+  },
+});
